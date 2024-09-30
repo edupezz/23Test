b588ee52006fbfcbf641dd9a7436dd7142a1ba96
Eduardo <eduardo.pezzi@fohat.co>
2024-09-30 00:32:52 -0300

    Created folder structure

---
+++ src/ui/atoms/Logo/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Logo = () => {
+
+    React.useEffect(() => {
+        console.log('logo rendered')
+    }, [])
+
+    return (
+        <>
+            logo
+        </>
+    )
+}
